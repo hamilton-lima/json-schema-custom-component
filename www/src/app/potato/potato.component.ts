55194b0dac44aa6e3082f0c762d1898b57eae581
Hamilton Lima <hamilton.lima@gmail.com>
2021-05-18 23:03:38 -0400

    Adds FormGroup when creating a new item - not updating the final value

--- www/src/app/potato/potato.component.ts
+++ www/src/app/potato/potato.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { JsonSchemaFormService } from '@ajsf/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+import { TileStyler } from '@angular/material/grid-list/tile-styler';
+
+interface INotes {
+  type: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-potato',
   templateUrl: './potato.component.html',
-  styleUrls: ['./potato.component.scss']
+  styleUrls: ['./potato.component.scss'],
 })
 export class PotatoComponent implements OnInit {
+  // copied from https://github.com/hamzahamidi/ajsf/blob/master/projects/ajsf-core/src/lib/widget-library/input.component.ts
+  formControl: AbstractControl;
+  controlName: string;
+  controlValue: INotes[];
+  controlDisabled = false;
+  boundControl = false;
+  options: any;
+  @Input() layoutNode: any;
+  @Input() layoutIndex: number[];
+  @Input() dataIndex: number[];
 
-  constructor() { }
+  // auto created by JSF
+  formArray: FormArray;
 
-  ngOnInit(): void {
+  constructor(
+    private jsf: JsonSchemaFormService,
+    private cdf: ChangeDetectorRef,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.options = this.layoutNode.options || {};
+    this.jsf.initializeControl(this);
+
+    this.formArray = this.jsf.formGroup.get(this.controlName);
+
+    // is a formControl created?
+    const value = this.jsf.formGroup.get(this.controlName).value;
+    console.log('created form control value', value);
+    console.log('is this a form array?', this.formArray);
   }
 
+  add() {
+    const newLine = { type: 'foo', text: 'something else' };
+
+    // pushes value
+    this.controlValue.push(newLine);
+
+    // update the form array to add a new line
+    const group = new FormGroup({
+      type: new FormControl(),
+      text: new FormControl(),
+    });
+    this.formArray.controls.push(group);
+
+    // updates value
+    this.jsf.updateValue(this, this.controlValue);
+  }
+
+  getValue() {
+    return this.controlValue;
+  }
 }

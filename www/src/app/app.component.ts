cc36d67eafeb9504dbc7ef0c47201e3dbc66c260
Hamilton Lima <hamilton.lima@gmail.com>
2021-05-18 20:47:39 -0400

    Adds angular material & bulma to the default view

--- www/src/app/app.component.ts
+++ www/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'www';
+  readonly codeLocation =
+    'https://github.com/hamilton-lima/json-schema-custom-component';
+
+  constructor(private router: Router) {}
+
+  code() {
+    window.open(this.codeLocation, '_blank');
+  }
+
+  home() {
+    this.router.navigate(['/home']);
+  }
 }
--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialAllComponentsModule } from './angular-material-all.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialAllComponentsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
